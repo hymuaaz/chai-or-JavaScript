59b279483fca0656a3a837e9c8d882d31eaa7d27
hymuaaz <128544607+hymuaaz@users.noreply.github.com>
2023-09-27 11:51:48 +0000

    container config done

---
+++ 01_basics/03_conversionsoperations.js
@@ -0,0 +1,26 @@
+let score = "13abc "
+// console.log(typeof score);
+
+let Value = Number(score);
+console.log(typeof Value);
+console.log(Value);
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1 ,
+// false = 0 
+
+let login = 1
+
+let booleanislogin = Boolean(login)
+console.log(booleanislogin)
+
+// 1 => true ,
+// 0 => false ,
+// "" => false ,
+// "muaaz" => true
+
+let somenumber = 33
+ 
+let check = String(somenumber)
+console.log(typeof check);
