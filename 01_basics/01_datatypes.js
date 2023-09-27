25fbeac4af79662c860b2db044911b6d62b33f4c
hymuaaz <128544607+hymuaaz@users.noreply.github.com>
2023-09-27 11:10:19 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+let namee = "muaaz"
+let age = 18
+let isLoggedIn = true
+
+console.table([namee , age , isLoggedIn])
+
+// number => 2 to power 53
+// string => ""
+// bigint 
+// boolean => true/false
+// null => standalone value 
+// undefined => undefined 
+// symbol => unique 
+// object 
+
+
+// console.log(typeof age );  number is type 
+// console.log(typeof "muaaz" );  string is type 
+// console.log(typeof null);   object is type 
+// console.log(typeof undefined);   undefined is type 
