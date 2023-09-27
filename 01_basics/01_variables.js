be08caa3945d1246c7b7eb8c4c477f272f9c59f1
hymuaaz <128544607+hymuaaz@users.noreply.github.com>
2023-09-27 09:45:50 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let email = "muaazaslam3@gmail.com"
+var password = "0000"
+
+let accountstate ;
+
+email = 'mianmuaaz'
+password = '1111'
+
+console.table([accountId , email , password , accountstate
+])
